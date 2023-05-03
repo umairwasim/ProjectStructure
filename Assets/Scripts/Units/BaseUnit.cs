b4747e051f83514a1ac800f2775f7093130d8d54
Umair <umairwasim001@gmail.com>
2023-05-03 23:47:50 +0500

    Base Unit class

---
+++ Assets/Scripts/Units/BaseUnit.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseUnit : MonoBehaviour
+{
+    public virtual void TakeDamage(int amount)
+    {
+
+    }
+}
