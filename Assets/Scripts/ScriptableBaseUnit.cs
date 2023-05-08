436a054e87c7935a581cc404e3d05ac0cea7fcad
Umair <umairwasim001@gmail.com>
2023-05-08 23:45:28 +0500

    Create ScriptableBaseUnit.cs

---
+++ Assets/Scripts/ScriptableBaseUnit.cs
@@ -0,0 +1,34 @@
+using System
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Base Unit", menuName ="Units")]
+public class ScriptableBaseUnit : ScriptableObject
+{
+    public Faction Faction;
+
+    [SerializeField] private Stats _stats;
+    public Stats BaseStats => _stats;
+
+    // Used in menus
+    public string Description;
+    public Sprite MenuSprite;
+}
+
+/// <summary>
+/// Keeping base stats as a struct on the scriptable keeps it flexible and easily editable.
+/// We can pass this struct to the spawned prefab unit and alter them depending on conditions.
+/// </summary>
+[Serializable]
+public struct Stats
+{
+    public int Health;
+    public int AttackPower;
+    public int TravelDistance;
+}
+
+[Serializable]
+public enum Faction
+{
+    Heroes = 0,
+    Enemies = 1
+}
