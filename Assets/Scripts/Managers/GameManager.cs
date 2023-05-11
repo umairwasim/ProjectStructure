b40b9b7b35a1d3dee8acb95ecd12d0d956f71456
Umair <umairwasim001@gmail.com>
2023-05-11 16:47:44 +0500

    Subtle changes

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -117,7 +117,7 @@ public class GameManager : MonoBehaviour
 
     private void HandleEnemyTurn()
     {
-
+        //AI intergration
     }
 
     private void BattleWon()
--- Assets/Scripts/Units/BaseUnit.cs
+++ Assets/Scripts/Units/BaseUnit.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseUnit : MonoBehaviour
+public class BaseUnit : ScriptableBaseUnit
 {
     public virtual void TakeDamage(int amount)
+    {
+
+    }
+
+    public virtual void Interact()
     {
 
     }
